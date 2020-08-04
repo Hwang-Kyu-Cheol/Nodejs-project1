4623942d81179a2c4b6d3397531eaef744bcedde
HKC <iambigman97@naver.com>
2020-08-04 16:30:15 +0900

    Title, Context, List by Function

--- main.js
+++ main.js
@@ -2,20 +2,16 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const app = http.createServer(function(request, response){
-    const rq_url = request.url;
-    const queryData = url.parse(rq_url, true).query;
-
-    let title = queryData.id;
-
-    if(rq_url === '/'){
-        title = 'Welcome';
+function makeFileList(fileArray){
+    let fileList = '<ul>';
+    for(let i = 0; i<fileArray.length; i++){
+        fileList = fileList + `<li><a href="/?id=${fileArray[i]}">${fileArray[i]}</a></li>`
     }
-    if(rq_url === '/favicon.ico'){
-        return response.writeHead(404);
-    }
+    fileList = fileList + '</ul>';
+    return fileList;
+}
 
-    response.writeHead(200);
+function makeHTML(title, context, fileList){
     const template = `
     <!DOCTYPE html>
     <html>
@@ -23,19 +19,48 @@ const app = http.createServer(function(request, response){
             <title>WEB - ${title}</title>
         </head>
         <body>
-            <h1>WEB</h1>
-            <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
+            <h1><a href="/">WEB</a></h1>
+            ${fileList}
             <hr>
             <h2>${title}</h2>
-            <p>Welcome to WEB World</p>
+            <p>${context}</p>
         </body>
     </html>
-    `
-    response.end(template);
+    `;
+    return template;
+}
+
+const app = http.createServer(function(request, response){
+    const rq_url = request.url;
+    const queryData = url.parse(rq_url, true).query;
+    const pathname = url.parse(rq_url, true).pathname;
+
+    if(pathname === '/'){
+        if(queryData.id === undefined){
+            fs.readdir('./data', function(error, fileArray){
+                const title = 'Welcome';
+                const context = 'Welcome to WEB';
+                const fileList = makeFileList(fileArray);
+                const template = makeHTML(title, context, fileList);
+                response.writeHead(200);
+                response.end(template); 
+            });
+        } else {
+            fs.readdir('./data', function(error, fileArray){
+                fs.readFile(`data/${queryData.id}.txt`, 'utf8', function(err, data){
+                    const title = queryData.id;
+                    const context = data;
+                    const fileList = makeFileList(fileArray);
+                    const template = makeHTML(title, context, fileList);
+                    response.writeHead(200);
+                    response.end(template);        
+                });
+            });
+        }
+    } else {
+        response.writeHead(404);
+        response.end('Not Found');          
+    }
 });
 
 app.listen(3000);
